dc647b2204562e4e7438a217c63347faef1f5149
nanka1570 <nanka1570@gmail.com>
2025-11-09 01:43:28 +0900

    js-practiceを追加

---
+++ js-practice/main.js
@@ -0,0 +1,16 @@
+'use strict'
+
+console.log(1 + 2);
+
+const result = 1 + 2;
+console.log(result);
+
+const myPokemon = "ピカチュウ";
+myPokemon = "ニャオハ";
+myPokemon = "ホゲータ";
+console.log(myPokemon);
+
+let myPokemon2 = "ピカチュウ";
+myPokemon = "ニャオハ";
+myPokemon = "ホゲータ";
+console.log(myPokemon);
