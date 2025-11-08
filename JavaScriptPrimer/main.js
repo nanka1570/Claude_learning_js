ce5932bf0a1483d90dad066c6d72a3703ee8b5aa
nanka1570 <nanka1570@gmail.com>
2025-11-09 01:33:57 +0900

    関数を学習（50%）、（ディレクトリを整理）

---
+++ JavaScriptPrimer/main.js
@@ -0,0 +1,127 @@
+'use strict'
+
+// console.log(typeof true);
+// console.log(typeof 42);
+// console.log(typeof 980071992540992n);
+// console.log(typeof true);
+// console.log(typeof "JavaScript"); 
+// console.log(typeof Symbol("シンボル"));
+// console.log(typeof undefined);
+// console.log(typeof null);
+// console.log(typeof ["配列"]);
+// console.log(typeof { "key": "value" }); 
+// console.log(typeof function() {});
+
+// console.log(typeof 'abc');
+
+// const str = "文字列";
+// console.log(`これは${str}です。`);
+// console.log(`これは\`コード\`です。`);
+
+// const foo = null;
+// console.log(foo);
+
+// const obj = {
+//     "key":"value"
+// };
+// console.log(obj.key);
+// console.log(obj["key"]);
+
+// const numberRegExp =  /\d+/;
+// console.log(numberRegExp.test("123"));
+
+// const str = new String("文字列")
+// const str = "文字列";
+// console.log(typeof str);
+// console.log(str.length);
+
+// console.log(10/0);
+
+// console.log(NaN == NaN);
+// console.log(typeof NaN);
+// console.log(Number.isNaN(NaN));
+
+// const obja = {},
+// objb = {};
+
+// console.log(obja === objb);
+// console.log(obja !== objb);
+
+// const value = undefined;
+// if(value === null || value === undefined){
+//     console.log("abc");
+// }
+// if(value == null){{
+//     console.log("def");
+// }}
+// if(value == undefined){
+//     console.log("ghi");
+// }
+
+// const array = [0, 1, 2];
+// const [a, b, c] = array;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// const obj = {
+//     "key":"value"
+// };
+// console.log(obj["key"]);
+// const {key} = obj;
+// console.log(key);
+
+// const user_name = "入力値" ?? "名前がありません。";
+
+// // 三項演算子
+// const score = 75;
+// const result = score >= 60 ? "合格" : "不合格";
+// console.log(result);
+
+// const num = 8;
+// const type = num % 2 == 0 ? "偶数" : "奇数";
+// console.log(type);
+
+// const sscore = 85;
+// const resultt = sscore >= 90 ? "A" : sscore >= 80 ? "B" : "C";
+// console.log(resultt);
+
+//8 関数
+function echo(x){
+    return x;
+}
+
+console.log(echo(1));
+console.log(echo());
+
+function argumentsToArray(x,y){
+    const z = x * y;
+    return z;
+}
+console.log(argumentsToArray(3,5));
+
+    //可変長引数
+    const max = Math.max(1, 5, 10, 20);
+    console.log(max);
+
+    //Rest parameters
+    function fn(...args){
+        console.log(args);
+    };
+    fn(1, 2, 3);
+
+    //Spread構文
+    const fnc = (a,b,c) => {
+        console.log(a);
+        console.log(b);
+        console.log(c);
+    }
+    const array = [1, 2, 3]
+    fnc(...array);
+
+    function arg(){
+        console.log(arguments[0]);
+        console.log(arguments[1]);
+        console.log(arguments[2]);
+    }
+    arg("a", "b", "c");
