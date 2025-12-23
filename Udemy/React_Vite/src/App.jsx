032df86cfcb758037bab676636b8fcd6b0e30f84
nanka1570 <nanka1570@gmail.com>
2025-12-23 22:17:12 +0900

    RefferenceError: Appコンポーネントをimportしようとした

---
+++ Udemy/React_Vite/src/App.jsx
@@ -0,0 +1,8 @@
+export const App = () => {
+    return (
+        <React.Fragment>
+            <h1>こんにちは</h1>
+            <p>お元気ですか？</p>
+        </React.Fragment>
+    );
+};
--- Udemy/React_Vite/src/index.jsx
+++ Udemy/React_Vite/src/index.jsx
@@ -1,6 +1,12 @@
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { App } from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(<h1>こんにちは</h1>);
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+    );
